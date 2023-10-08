66f209d03425954f28a58ec0900bfe3ee7a76ba3
dinas <dalsalahy@ivytech.edu>
2023-10-08 15:56:31 -0400

    all MVC complete

--- Dragons/routes/coursesRoutes.js
+++ Dragons/routes/coursesRoutes.js
@@ -1,63 +1,26 @@
 const express = require('express');
-//const blogController = require('../controllers/blogController')
-const Course = require('../models/course')
+const coursesController = require('../controllers/coursesControllers')
+
 
 const router = express.Router();
  
    
  
  //Courses Pages
- router.get ('/add' , (req, res) => {
-    res.render('addCourse' , { title : 'Add Course'}); 
- });   
+ router.get ('/add' , coursesController.course_create_get);
 
  //getting all courses list
- router.get ('/coursesIndex' , (req, res) => { 
-    Course.find().sort({ createdAt: -1 })
-       .then((result) => {
-          res.render('coursesIndex', { title: 'All Courses', courses: result })//geting all courses details and send result to course Index page
-       })
-       .catch((err) => {
-          console.log(err);
-       }); 
- });
+ router.get ('/coursesIndex' , coursesController.courses_index );
  
  // Adding 1 course to DB
- router.post('/add', (req, res) => {
-    const course = new Course(req.body);
-    course.save()
-       .then((result) => {
-          res.redirect('./coursesIndex');
-       })
-       .catch((err) => {
-          console.log(err);
-       })
- });
+ router.post('/add', coursesController.course_create_post);
  
+
  //Getting one course details and send it to Course Details page
- router.get('/:id',  (req, res) => {
-    const id = req.params.id;   
-    Course.findById(id)
-     .then(result => {
-       res.render('courseDetails', { course: result, title: 'Course Details' });
-     })
-     .catch(err => {
-       console.log(err);
-     });
- });
+ router.get('/:id',  coursesController.course_details);
  
  //Deleting one course
- router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Course.findByIdAndDelete(id)
-      .then(result => {
-        res.json({ redirect: './coursesIndex' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+ router.delete('/:id', coursesController.course_delete);
  
 
   module.exports = router;
