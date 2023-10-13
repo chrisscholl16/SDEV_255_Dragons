55143bf6bf18201174292c0f1226fc9c31d8133b
dinas <dalsalahy@ivytech.edu>
2023-10-12 20:52:53 -0400

    student, add, check , delete courses from schedule

--- Dragons/controllers/studentsControllers.js
+++ Dragons/controllers/studentsControllers.js
@@ -17,7 +17,8 @@ const Student = require('../models/Student');
            courseCredit: course.credits }]} }
    )
        .then((result) => {
-          res.redirect('/courses/coursesIndex');
+         // res.redirect('/courses/coursesIndex');
+         res.redirect('/students/showSchedule/'+course.studentId);
        })
        .catch((err) => {
           console.log(err);
@@ -26,7 +27,6 @@ const Student = require('../models/Student');
 
  //controller to show students schedule
  const  student_showShedule_get = (req, res) => {  
-
    const id = req.params.id;   
    Student.findById(id)
     .then(result => {
@@ -37,6 +37,21 @@ const Student = require('../models/Student');
     });
 };
 
+//Deleting one course
+const course_in_schedule_delete = (req, res) => {
+  console.log(req.params);
+   const id = req.params.id;
+   
+   Student.updateOne({ _id: id },{ $pull: { schedule: {courseId:req.body.courseId} } }) 
+   
+   .then((result) => {    
+    res.redirect('/students/showSchedule/'+id);
+ })
+     .catch(err => {
+       console.log(err);
+     });
+ }
+
 
 
 
@@ -45,5 +60,6 @@ module.exports={
 
         
         course_addToSchedule_post,
-        student_showShedule_get
+        student_showShedule_get,
+        course_in_schedule_delete
 }
--- Dragons/routes/studentsRoutes.js
+++ Dragons/routes/studentsRoutes.js
@@ -4,10 +4,14 @@ const studentsController = require('../controllers/studentsControllers')
 const router = express.Router();   
  
 //Student retrieving his schedule
+
 router.get('/showSchedule/:id', studentsController.student_showShedule_get);
 
  //Student adding course to schedule
  router.post('/addToSchedule', studentsController.course_addToSchedule_post);
+
+ //Deleting one course
+ router.post('/deleteFromSchedule/:id', studentsController.course_in_schedule_delete);
 
 
 
